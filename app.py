99c6f86c6de5f8327edd49de1abdbdd65d81df48
eric <ericmwangi82@gmail.com>
2021-06-22 10:02:58 +0300

    commenting on some line of code in the app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ from db import db
 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['POSTGRESQL_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+app.config['POSTGRESQL_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'prodigious'
 api = Api(app)
 db.init_app(app)
